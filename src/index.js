b05e680ccdc777706059afb25ba2765d0dffae7f
FernandoRhenan <fernandorhenan9@gmail.com>
2023-09-03 18:52:01 -0300

    Simplificando configuração Docker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+const prisma = require('./utils/PrismaInstance')
 
-app.get("/", function(req, res) {
-    res.send("<h1>Servidor rodando com ExpressJS</h1>");
+app.post("/", function (req, res) {
+	res.send('ok');
 });
 
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
